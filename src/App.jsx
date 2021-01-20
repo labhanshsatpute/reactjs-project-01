21d3d3c27c87faf5ab0eaca1101f2d69399531c9
Sahil Rathee RTTSS <sahil.git@outlook.com>
2021-01-20 18:34:00 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import Navbar from './Navbar';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import './colors.css';
-import SectionHome from './SectionHome';
-import SectionAbout from './SectionAbout';
-import SectionServices from './SectionServices';
-import SectionPortfolio from './SectionPortfolio';
-import SectionTeam from './SectionTeam';
-import SectionContact from './SectionContact';
-import Footer from './Footer';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React from "react";
+import Navbar from "./Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "./colors.css";
+import SectionHome from "./SectionHome";
+import SectionAbout from "./SectionAbout";
+import SectionServices from "./SectionServices";
+import SectionPortfolio from "./SectionPortfolio";
+import SectionTeam from "./SectionTeam";
+import SectionContact from "./SectionContact";
+import Footer from "./Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
-	AOS.init({
-		offset: 150,
-	});
-	return (
-	<React.Fragment>
-		<Navbar/>
-		<main>
-			<SectionHome/>
-			<SectionAbout/>
-			<SectionServices/>
-			<SectionPortfolio/>
-			<SectionTeam/>
-			<SectionContact/>
-		</main>
-		<Footer/>
-	</React.Fragment>
-	);
-};
+  AOS.init({
+    offset: 150,
+  });
+  return (
+    <React.Fragment>
+      <Navbar />
+      <main>
+        <SectionHome />
+        <SectionAbout />
+        <SectionServices />
+        <SectionPortfolio />
+        <SectionTeam />
+        <SectionContact />
+      </main>
+      <Footer />
+    </React.Fragment>
+  );
+}
 
 export default App;
